091cef5f76fc120cc59c9001bb07cfc117d97ad4
Javi-codehub <jarikre34@gmail.com>
2025-05-16 10:35:08 -0700

    javascript-tutorials

---
+++ Hello_World/script.js
@@ -0,0 +1,96 @@
+                // // this is for numbers//
+// let arr =[1,2,3];
+// arr[0]=9; //modifies the array
+// console.log(arr); // [9,2,2] (changed)
+
+               // //this is to change to uppercase//
+// let str="hello world";
+// console.log(str);
+// str = str.toUpperCase();
+// console.log(str);
+
+// // this is for loop//
+// let fruits = ["apple","orange","banana" ];
+// for (let i=0; i< fruits.length; i++) {
+//     console.log(fruits[i])
+// }
+
+// //*****this is while loop ******//
+// let dice = Math.ceil(Math.random()*6);
+// console.log(dice);
+// let count = 1;
+// while (dice != 6) {
+//     dice = Math.ceil(Math.random()*6);
+//     console.log(dice);
+//     cout += 1;
+// }
+
+// console.log('You got 6!!');
+// console.log('You rolled', count,'times');
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+// // let user = {
+//     name: "javi",
+//     age: 25,
+//     email:'jarikre34@gmail.com',
+//     school: function (){
+//     return "i am a student"jm;
+
+// }
+// }
+
+// console.log(user.name);//output; javi
+
+// console.log(user.school());
+// // 
+
+//
+// let book = {
+//     title: "light of the world",
+//     author: "javi",
+//     yearpublished: 2025,
+//     isavailable: true,
+//     borrowbook: function(){
+//         this.isavailable = false
+//     return this.isavailable;
+//     }
+// }
+// console.log(book.borrowbook()) 
+// //
+// let person = {
+//     name:"joshau",
+//     age: 29,
+//     city: "Enugu",
+//     introduce: function(){
+//         return (`hi, im ${person.name}, im ${person.age} years old, and i live in ${person.city}`)
+//     }
+// }
+// console.log(person.introduce())
+
+        // this is TYPEOF //
+// var firstname = 'Joshua';
+// var lastname = 'Jarikre';
+// var age = 24
+// var driversLisence = true
+// var dreamJob
+// console.log()
+// var dreamJob = "developer"
+
+// console.log(typeof firstname);
+// console.log(typeof lastname);
+// console.log(typeof age);
+// console.log(typeof driversLisence);
+// console.log(typeof dreamJob);
+
+
+var markAge = 24;
+var joshuaAge = 30;
+var calebAge = 39;
+var javiAge = 31;
+var nonsoAge = 26;
+
+var averageAge = (markAge + joshuaAge + calebAge + javiAge + nonsoAge) / 5;
+console.log ("the average age is:" + averageAge);
